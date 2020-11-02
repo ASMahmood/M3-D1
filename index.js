bbd2cab0233e14a75c617c73db468280d2fd78b7
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-11-02 13:30:34 +0000

    update ex6-10

--- index.js
+++ index.js
@@ -91,27 +91,85 @@ console.log(ex5(95, 77));
 6) 
 Create a function to create a new string of specified copies (positive number) of a given string.
 */
+console.log("---------EX6---------");
+
+const ex6 = function (x) {
+  const ex6String = "Hello World";
+  let ex6Array = [];
+  for (let i = 0; i < x; i++) {
+    ex6Array.push(ex6String);
+  }
+  return ex6Array.join("");
+};
+
+console.log(ex6(2));
 
 /*
 7)
 Create a function to display the city name if the string begins with "Los" or "New" otherwise return blank.
 */
+console.log("---------EX7---------");
+
+const ex7 = function (str) {
+  if (str.substring(0, 3) === "Los" || str.substring(0, 3) === "New") {
+    return str;
+  } else {
+    return "blank";
+  }
+};
+
+console.log(ex7("Los Santos"));
 
 /*
 8)
 Create a function to calculate the sum of three elements of a given array of integers of length 3.
 */
+console.log("---------EX8---------");
+
+const ex8 = function (arr) {
+  const sum = arr[0] + arr[1] + arr[2];
+  return sum;
+};
+
+console.log(ex8([8, 6, 9]));
 
 /*
 9)
 Create a function to test whether an array of integers of length 2 contains 1 or a 3. 
 */
+console.log("---------EX9---------");
+
+const ex9 = function (arr) {
+  if (arr[0] === 1 || arr[0] === 3 || arr[1] === 1 || arr[1] === 3) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(ex9([2, 4]));
 
 /*
 10)
 Create a function to test whether an array of integers of length 2 does not contain 1 or a 3
+*/
+console.log("---------EX10---------");
+
+const ex10 = function (arr) {
+  if (!(arr[0] === 1 || arr[0] === 3 || arr[1] === 1 || arr[1] === 3)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(ex10([2, 3]));
+
+/*
 11)
 Create a function to find the longest string from a given array of strings.
+*/
+/*
 12)
 Create a function to find the types of a given angle.
 Types of angles:
@@ -119,18 +177,32 @@ Types of angles:
     Right angle: An 90 degree angle.
     btuse angle: An angle between 90 and 180 degrees.
     Straight angle: A 180 degree angle.
+*/
+/*
 13)
 Create a function to find the index of the greatest element of a given array of integers
+*/
+/*
 14)
 Create a function to get the largest even number from an array of integers.
+*/
+/*
 15)
 Create a function to check two given numbers and return true if one of the number is 50 or if their sum is 50.
+*/
+/*
 16)
 Create a function to check from two given integers, whether one is positive and another one is negative.
+*/
+/*
 17)
 Create a function to create new string with first 3 characters are in lower case and the others in upper case. If the string length is less than 3 convert all the characters in upper case. 
+*/
+/*
 18)
 Create a function to calculate the sum of the two given integers, If the sum is in the range 50..80 return 65 other wise return 80.
+*/
+/*
 19)
 Create a function to convert a number to a string, the contents of which depend on the number's factors. Follow next example:
 If the number has 3 as a factor, output 'Diego'.
@@ -144,6 +216,8 @@ this would be a simple "Stefano".
 this would be a "DiegoRiccardo".
 34 has four factors: 1, 2, 17, and 34.
 this would be "34".
+*/
+/*
 20)
 Create a function that given a phrase returns its acronym, like British Broadcasting Corporation returns BBC
 */
